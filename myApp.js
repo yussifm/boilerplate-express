b0c834dcd31a6e98d6fd5a9f6bcc8e4faef56b69
yussif mohammed <43506617+yussifm@users.noreply.github.com>
2021-04-14 13:43:38 +0000

    sta

--- myApp.js
+++ myApp.js
@@ -5,7 +5,7 @@ console.log("Hello world");
 
 
 const path = require('path');
-app.use(path.join(__dirname, express.static('public/style.css')));
+app.use("/public", express.static(__dirname + "/public"));
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname,"/views/index.html"));
     
