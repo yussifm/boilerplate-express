ed64484694e311af51db1dc29264db59156b72b0
yussif mohammed <43506617+yussifm@users.noreply.github.com>
2021-04-14 13:50:06 +0000

    json

--- myApp.js
+++ myApp.js
@@ -1,51 +1,17 @@
-var express = require('express');
+var express = require("express");
 var app = express();
 
-console.log("Hello world"); 
+console.log("Hello world");
 
-
-const path = require('path');
+const path = require("path");
 app.use("/public", express.static(__dirname + "/public"));
-app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname,"/views/index.html"));
-    
-})
-app.get('json', (req, res) => {
-    res.json({"message": "Hello json"})
-})
+app.get("/", function (req, res) {
+	res.sendFile(path.join(__dirname, "/views/index.html"));
+});
+app.get("/json", (req, res) => {
+	res.json({
+		message: "Hello json",
+	});
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
