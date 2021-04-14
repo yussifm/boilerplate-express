3eb0880b4fad73d203b2fd8d84304ff6bb2f6236
yussif mohammed <43506617+yussifm@users.noreply.github.com>
2021-04-14 13:29:32 +0000

    file

--- myApp.js
+++ myApp.js
@@ -3,6 +3,8 @@ var app = express();
 
 console.log("Hello world"); 
 
+
+const path = require('path');
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname,"/views/index.html"));
     
