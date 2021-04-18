a0ac892d3d09f2534f34cd5051dbd2e97838249b
yussif mohammed <43506617+yussifm@users.noreply.github.com>
2021-04-18 00:59:40 +0000

    env

--- myApp.js
+++ myApp.js
@@ -8,10 +8,17 @@ app.use("/public", express.static(__dirname + "/public"));
 app.get("/", function (req, res) {
 	res.sendFile(path.join(__dirname, "/views/index.html"));
 });
+
+
+if (process.env.MESSAGE_STYLE === 'uppercase')
+{
+	
 app.get("/json", (req, res) => {
 	res.json({
-		message: "Hello json",
+		message: "HELLO JSON",
 	});
 });
+}
+
 
 module.exports = app;
